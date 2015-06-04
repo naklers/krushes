d83a26d3dee699afb2f42c7a3f91b4f2a80d65e5
Ignacio Naveiras <inaveiras2015@kellogg.northwestern.edu>
2015-06-04 17:01:43 -0500

    v0.1.2
    Updated password page
    Updated title
    Created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user_hashes = [
+  { :name => "Ignacio Naveiras", :email => "inaveiras2015@kellogg.northwestern.edu", :password => "12341234"  },
+  { :name => "Andrea Beer", :email => "asilvabeer2015@kellogg.northwestern.edu", :password => "12341234" },
+  { :name => "Santi Otaola", :email => "sotaolaarca2015@kellogg.northwestern.edu", :password => "12341234" },
+  { :name => "Emily Gipple", :email => "egipple2015@kellogg.northwestern.edu", :password => "12341234" },
+  { :name => "Paloma Ishii", :email => "pishii2015@kellogg.northwestern.edu", :password => "12341234" },
+  { :name => "Pedro Haces", :email => "phaces2016@kellogg.northwestern.edu", :password => "12341234" },
+  { :name => "Pablo Jaramillo", :email => "pjaramillo2016@kellogg.northwestern.edu", :password => "12341234" },
+  { :name => "Eyal Klein", :email => "eklein2015@kellogg.northwestern.edu", :password => "12341234" },
+  { :name => "Jason Yeh", :email => "jyeh2015@kellogg.northwestern.edu", :password => "12341234" }
+]
+
+user_hashes.each do |user_hash|
+  User.create(user_hash)
+end
+
+puts "There are now #{User.count} users in the database."

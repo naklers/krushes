83a8323b7a02760e0918a0bae0f15d2930a5bea6
Ignacio Naveiras <inaveiras2015@kellogg.northwestern.edu>
2015-06-06 13:47:20 -0500

    warning
    warning

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'csv'
+# require 'csv'
 
-csv_text = File.read("./users3.csv")
-csv = CSV.new(csv_text, :headers => true, :header_converters => :symbol)
-csv.each do |row|
-  user_hash = row.to_hash
-  generated_password = Devise.friendly_token.first(6).downcase.to_s
-  user_hash[:password] = generated_password
-  user = User.create(user_hash)
-  puts("CREATED USER " + user.name.to_s + " with pwd " + user_hash[:password].to_s)
-  # Ask mailer to send welcome email as part of the user creation
-  UserMailer.welcome_email(user,generated_password).deliver!
-end
+# csv_text = File.read("./users3.csv")
+# csv = CSV.new(csv_text, :headers => true, :header_converters => :symbol)
+# csv.each do |row|
+#   user_hash = row.to_hash
+#   generated_password = Devise.friendly_token.first(6).downcase.to_s
+#   user_hash[:password] = generated_password
+#   user = User.create(user_hash)
+#   puts("CREATED USER " + user.name.to_s + " with pwd " + user_hash[:password].to_s)
+#   # Ask mailer to send welcome email as part of the user creation
+#   UserMailer.welcome_email(user,generated_password).deliver!
+# end
 
-puts "There are now #{User.count} users in the database."
+# puts "There are now #{User.count} users in the database."

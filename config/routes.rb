0b1f20174b5b1a08ac786d7cb603d2c92237a1de
Ignacio Naveiras <inaveiras2015@kellogg.northwestern.edu>
2015-11-09 00:57:00 -0800

    Update photo without password
    Extended Devise's Users controller to allow updating profile picture
    without password.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   root 'choices#index'
 

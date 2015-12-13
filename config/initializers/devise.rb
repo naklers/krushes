21f7f8996c502a5834edcdfbd8b40e648d3c0a69
Ignacio Naveiras <inaveiras2015@kellogg.northwestern.edu>
2015-12-13 12:57:54 -0800

    Renamed to Krushes
    Renamed to Krushes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,19 +24,10 @@ Devise.setup do |config|
   require 'devise/orm/active_record'
 
   # ==> Configuration for any authentication mechanism
-  # Configure which keys are used when authenticating a user. The default is
-  # just :email. You can configure it to use [:username, :subdomain], so for
-  # authenticating a user, both parameters are required. Remember that those
-  # parameters are used only when authenticating and not when retrieving from
-  # session. If you need permissions, you should implement that in a before filter.
-  # You can also supply a hash where the value is a boolean determining whether
-  # or not authentication should be aborted when the value is not present.
+  # Configure which keys are used when authenticating a user. The default is just :email. You can configure it to use [:username, :subdomain], so for # authenticating a user, both parameters are required. Remember that those parameters are used only when authenticating and not when retrieving from session. If you need permissions, you should implement that in a before filter. You can also supply a hash where the value is a boolean determining whether or not authentication should be aborted when the value is not present.
   config.authentication_keys = [:email]
 
-  # Configure parameters from the request object used for authentication. Each entry
-  # given should be a request method and it will automatically be passed to the
-  # find_for_authentication method and considered in your model lookup. For instance,
-  # if you set :request_keys to [:subdomain], :subdomain will be used on authentication.
+  # Configure parameters from the request object used for authentication. Each entry given should be a request method and it will automatically be passed to the find_for_authentication method and considered in your model lookup. For instance, if you set :request_keys to [:subdomain], :subdomain will be used on authentication.
   # The same considerations mentioned for authentication_keys also apply to request_keys.
   # config.request_keys = []
 
